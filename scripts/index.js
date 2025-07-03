4891a92450538480e9f75a75deba562f2e10426a
Ailén Sosa <ailenjazminsosa@gmail.com>
2025-07-02 23:08:27 -0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,1 @@
-'use strict';
 
-/*
- * APELLIDO, NOMBRE | APELLIDO, NOMBRE
- */
-
-// Podria levantar los productos de un JSON o directamente estar escritos aca
-
-let productos = [
-    // {
-    //     id: 1,
-    //     nombre: 'Producto',
-    //     descripcion: 'Descripción del producto',
-    //     precio: 0,
-    //     imagen: 'images/producto-de-ejemplo.jpg',
-    //     categoría: 'Nombre de la categoría',
-    // }
-];
